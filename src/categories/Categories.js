8643a55bad3da4e158d5a10d7b1576d29cd50bff
Grant Patterson <darkrider231@gmail.com>
2021-04-20 12:21:38 -0400

    Created all components and added styling to the App. Letter, and Timer components

---
+++ src/categories/Categories.js
@@ -0,0 +1,22 @@
+
+
+const Categories = () => {
+    return (
+        <div className='Categories'>
+            <div className='Categories-Main'>
+                <div className='Categories-Main-Top'>
+                    <h1>CATEGORIES</h1>
+                    <h1>List 1</h1>
+                 </div>
+                 <ol className="Categories-Main-Display">
+                     <li>A Boy's Name</li>
+                 </ol>
+            </div>
+            <div className="Categories-Seconday">
+                <h1 className='Categories-Seconday-H1'>NEXT LIST</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Categories;
