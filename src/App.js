36feccfb0b2c8e764c7de8f97185bc3889967621
Grant Patterson <darkrider231@gmail.com>
2021-04-23 11:31:34 -0400

    ScattergoriesGame Project Completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Categories from './categories/Categories';
 import Letter from './letter/Letter';
 import Timer from './timer/Timer';
 
 function App() {
+  const [hide, setHide] = useState(true);
 
   return (
     <div className="App">
       <div className='App-Section-One'>
       <Letter />
-      <Timer />
+      <Timer setHide={setHide}/>
       </div>
 
       <div className='App-Section-Two'>
-      <Categories />
+      <Categories  hide={hide} setHide={setHide}/>
       </div>
     </div>
   );
